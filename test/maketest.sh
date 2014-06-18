aa7423776b5abbc63ea1fb6697abf56ae9bee437
Sandro Wenzel <sandro.wenzel@cern.ch>
2014-06-18 09:45:28 +0200

    slight change to makefile for test

--- test/maketest.sh
+++ test/maketest.sh
@@ -1,1 +1,1 @@
-g++ -g -O2 -ftree-vectorizer-verbose=1 -ffast-math  -I ${G4INCLUDE} testPropagateMagField.cc `geant4-config --libs`
+g++ -g -O2 -ftree-vectorize -ftree-vectorizer-verbose=1  -I ${G4INCLUDE} testPropagateMagField.cc `geant4-config --libs`
