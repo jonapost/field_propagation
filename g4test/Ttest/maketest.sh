5a48e7843d8a69184088fd910abff93aa9041687
quarkxie <qxie@coe.com>
2014-07-04 16:46:34 +0800

    option

--- g4test/Ttest/maketest.sh
+++ g4test/Ttest/maketest.sh
@@ -1,1 +1,1 @@
-g++ -DINLINEDUMBSTEPPERS -o testPropagateMagField -g -O2 -ftree-vectorize -ftree-vectorizer-verbose=0  -I ${G4INCLUDE} testPropagateMagField.cc `geant4-config --libs` 
+g++ -o testPropagateMagField -g -O2 -ftree-vectorize -ftree-vectorizer-verbose=0  -I ${G4INCLUDE} testPropagateMagField.cc `geant4-config --libs` 
