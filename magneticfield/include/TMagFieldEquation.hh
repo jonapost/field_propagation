c2e7a6e4fe01045d3ca94343e7bcc98d6989945f
quarkxie <qxie@coe.com>
2014-07-09 13:55:07 +0800

    minor

--- magneticfield/include/TMagFieldEquation.hh
+++ magneticfield/include/TMagFieldEquation.hh
@@ -40,7 +40,8 @@ class TMagFieldEquation : public G4Mag_UsualEqRhs
             void RightHandSide(const G4double y[], G4double dydx[] )
             //	const
             {
-                G4double Field[G4maximum_number_of_field_components]; 
+                G4double Field
+                    [G4maximum_number_of_field_components]; 
                 G4double  PositionAndTime[4];
                 PositionAndTime[0] = y[0];
                 PositionAndTime[1] = y[1];
@@ -48,29 +49,32 @@ class TMagFieldEquation : public G4Mag_UsualEqRhs
                 PositionAndTime[3] = y[7];   
                 GetFieldValue(PositionAndTime, Field) ;
 
-                Blaze3DVec yv(y[3],y[4],y[5]);
+                //copy first 8th element
+                Blaze8DVec yv(8UL, y);
                 Blaze3DVec Fieldv(3UL, Field);
-                Blaze8DVec dydxv( TEvaluateRhsGivenB(yv, Fieldv) );
+                Blaze8DVec dydxv(
+                        TEvaluateRhsGivenB(
+                            subvector(yv, 3UL, 3UL), 
+                            Fieldv) );
 
                 for (int i = 0; i < 6; i ++)
                 { dydx[i] = dydxv[i]; }
             }
 
         __attribute__((always_inline)) 
-            Blaze8DVec TEvaluateRhsGivenB( const Blaze3DVec y,
-			                                  const Blaze3DVec B ) const
+            Blaze8DVec TEvaluateRhsGivenB( const Blaze3DVec y, 
+                    const Blaze3DVec B ) const
             {
                 Blaze8DVec dydxv;
                 
                 //dot product
                 G4double momentum_mag_square = (y, y);
                 
-                G4double inv_momentum_magnitude = vdt::fast_isqrt_general( momentum_mag_square, 4);
+                G4double inv_momentum_magnitude = 
+                    vdt::fast_isqrt_general( momentum_mag_square, 4);
                 G4double cof = FCof()*inv_momentum_magnitude;
                 
-                //scalar product
-                subvector(dydxv, 0UL, 3UL) = 
-                    inv_momentum_magnitude*y;
+                subvector(dydxv, 0UL, 3UL) = inv_momentum_magnitude*y;
 		          
                 //cross product
                 subvector(dydxv, 3UL, 3UL) = cof*( y % B );
