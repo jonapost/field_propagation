5780c1450e5b72b78cc35d83396137cb57841cbf
Jason Suagee <jsuagee@gmail.com>
2015-05-28 11:41:57 -0400

    Added revised ChawlaSharmaRKNstepper (more readable version). Changed testPropagateMagField.cc: added case 19 for ChawlaSharmaRKNstepper

---
+++ magneticfield/include/ChawlaSharmaRKNstepper.hh
@@ -0,0 +1,80 @@
+#ifndef CHAWLA_SHARMA_RKN_STEPPER_HH
+#define CHAWLA_SHARMA_RKN_STEPPER_HH
+
+#include "globals.hh"
+#include "G4MagIntegratorStepper.hh"
+#include "G4Mag_EqRhs.hh"
+#include "G4ThreeVector.hh"
+#include "G4LineSection.hh"
+
+class ChawlaSharmaRKNstepper : public G4MagIntegratorStepper
+{
+	public:
+	ChawlaSharmaRKNstepper(G4Mag_EqRhs *EquationMotion,
+					G4int numberOfVariables = 6, G4int numStateVariables = 12);
+	
+	~ChawlaSharmaRKNstepper();
+	
+	void Stepper( const G4double y[],
+                  const G4double dydx[],
+                        G4double h,
+                        G4double yout[],
+                        G4double yerr[]  );
+	
+	void DumbStepper( const G4double y[],
+										//const G4double dydx[],
+										G4double h,
+										G4double yOut[] );
+	
+	G4int IntegratorOrder() const {return 3;}; // Richardson extrapolation promotes to 4th order
+   
+   G4double DistChord() const;
+   
+   void EvaluateRhs( const G4double y[],
+				           G4double dmom[] ) const;
+	
+	private:
+   
+   ////////////////////////////////////////////////////////////////
+   // Private data
+   ////////////////////////////////////////////////////////////////
+
+   G4Mag_EqRhs*           m_fEq;          
+   
+   private:
+   
+   // STATE
+   G4ThreeVector fInitialPoint, fMidPoint, fFinalPoint;
+   // Data stored in order to find the chord
+   
+   // Dependent Objects, owned --- part of the STATE 
+   G4double *yInitial, *yMiddle, *dydxMid, *yOneStep;
+   // The following arrays are used only for temporary storage
+   // they are allocated at the class level only for efficiency -
+   // so that calls to new and delete are not made in Stepper().
+};
+
+
+inline
+ChawlaSharmaRKNstepper::ChawlaSharmaRKNstepper(G4Mag_EqRhs *EquationRhs,
+                                     G4int numberOfVariables, 
+				     G4int numStateVariables)
+     : G4MagIntegratorStepper(EquationRhs,numberOfVariables,numStateVariables)
+  {
+      G4int nvar = std::max(this->GetNumberOfVariables(), 8);
+      yMiddle=     new G4double[nvar]; 
+      dydxMid=     new G4double[nvar];
+      yInitial=    new G4double[nvar];
+      yOneStep= new G4double[nvar];
+      m_fEq = EquationRhs;
+  }
+
+
+#endif  //CHAWLA_SHARMA_RKN_STEPPER_HH
+
+
+
+
+
+
+
---
+++ magneticfield/src/ChawlaSharmaRKNstepper.cc
@@ -0,0 +1,221 @@
+#include "ChawlaSharmaRKNstepper.hh"
+
+ChawlaSharmaRKNstepper::~ChawlaSharmaRKNstepper()
+{
+   delete[] yMiddle;
+   delete[] dydxMid;
+   delete[] yInitial;
+   delete[] yOneStep;
+}
+
+void ChawlaSharmaRKNstepper::Stepper(  const G4double yInput[],
+		            const G4double dydx[],
+		                  G4double hstep,
+		                  G4double yOutput[],
+		                  G4double yError [] ){
+		                  
+   const G4int nvar = this->GetNumberOfVariables() ;
+   const G4int maxvar= GetNumberOfStateVariables();
+
+   G4int i;
+   // correction for Richardson Extrapolation.
+   G4double  correction = 1. / ( (1 << IntegratorOrder()) -1 );
+   
+   //  Saving yInput because yInput and yOutput can be aliases for same array
+
+   for(i=0;i<nvar;i++) yInitial[i]=yInput[i];
+   yInitial[7]= yInput[7];    // Copy the time in case ... even if not really needed
+   yMiddle[7] = yInput[7];  // Copy the time from initial value 
+   yOneStep[7] = yInput[7]; // As it contributes to final value of yOutput ?
+   // yOutput[7] = yInput[7];  // -> dumb stepper does it too for RK4
+   for(i=nvar;i<maxvar;i++) yOutput[i]=yInput[i];
+   // yError[7] = 0.0;         
+   
+   G4double halfStep = hstep * 0.5; 
+   
+   // Comment (*):
+   // Do two half steps 
+   
+   G4double m_mom = std::sqrt(yInitial[3]*yInitial[3]+yInitial[4]*yInitial[4]+yInitial[5]*yInitial[5]);
+   for(i = 3; i < 6; i++) yInitial[i] /= m_mom;
+   
+   DumbStepper  (yInitial, halfStep, yMiddle);
+   
+   //RightHandSide(yMiddle, dydxMid);
+   
+   // Comment (**):
+   // Function evaluations f(x,y,y') are not at (x_k, y_k, y'_k)
+   // but rather at (x_k, y_k + alpha1*halfStep*y'_k, y'_k).
+   // There is no point in calculating dy/dx at yMiddle.
+   // See p. 376 of Chawla/Sharma.
+   
+   DumbStepper  (yMiddle, halfStep, yOutput); 
+
+   // Store midpoint, chord calculation
+
+   fMidPoint = G4ThreeVector( yMiddle[0],  yMiddle[1],  yMiddle[2]); 
+
+   // Do a full Step
+   DumbStepper(yInitial, hstep, yOneStep);
+   for(i=0;i<nvar;i++) {
+      yError [i] = yOutput[i] - yOneStep[i] ;
+      yOutput[i] += yError[i]*correction ;  // Provides accuracy increased
+                                            // by 1 order via the 
+                                            // Richardson Extrapolation.
+                                            // Question: Is the order reported as 3 to MagIntegratorDriver
+                                            // ( at MagIntegratorDriver.cc line 90 )
+                                            // when it should be reported as 3+1 = 4 ?
+   }
+   
+   // Now renomalize momentum
+   G4double momentum_sqrt = std::sqrt( yOutput[3]*yOutput[3] + yOutput[4]*yOutput[4] + yOutput[5]*yOutput[5] );
+   
+   for(i = 3; i < 6; i ++){
+      yOutput[i] /= momentum_sqrt;
+   }
+   
+   fInitialPoint = G4ThreeVector( yInitial[0], yInitial[1], yInitial[2]); 
+   fFinalPoint   = G4ThreeVector( yOutput[0],  yOutput[1],  yOutput[2]); 
+
+   return ;
+}
+
+void ChawlaSharmaRKNstepper::DumbStepper( 
+										const G4double yIn[],
+										// const G4double dydx[],
+										G4double Step,
+										G4double yOut[] ) {
+	// dydx is not even used by DumbStepper. See Comment (**) above.
+	
+	G4int i;
+	G4double K1[3], K2[3], K3[3];
+	G4double pos[3], mom[3], t;
+	G4double temp_eval_pt[7];
+	//G4double m_mom_sqrd = 0., m_mom;
+	/*const G4double   a1 = 1./4. , a2 = 0., a3 = 1./4.,
+ 									b1 = 1./4., b2 = 0., b3 = 3./4.,
+ 									alpha1 = 0., alpha2 = 2./3., alpha3 = 2./3.,
+ 									beta21 = -1./9., beta31 = 2./9., beta32 = 0.,
+ 									gamma21 = 2./3., gamma31 = 1./3., gamma32 = 1./3.;
+ 	*/
+   
+   const G4double   a1 = 0. , a2 = 1./2., a3 = 0.,
+ 									b1 = 0., b2 = 3./4., b3 = 1./4.,
+ 									alpha1 = 1./3., alpha2 = 1./3., alpha3 = 1.,
+ 									beta21 = 0., beta31 = 1., beta32 = -1./3.,
+ 									gamma21 = 1./3., gamma31 = -1., gamma32 = 2.;
+   
+   // Initialise time to t0, needed when it is not updated by the integration.
+   //        [ Note: Only for time dependent fields (usually electric) 
+   //                  is it neccessary to integrate the time.] 
+   yOut[7] = temp_eval_pt[7] = yIn[7]; // I suppose still do it this way??
+   yOut[6] = temp_eval_pt[6] = yIn[6];
+   // const G4int numberOfVariables= this->GetNumberOfVariables(); 
+   // The number of variables to be integrated over
+   // Does this actually vary??
+   //  Saving yInput because yInput and yOut can be aliases for same array
+   
+   for(i = 0; i < 3; i ++){
+      pos[i] = yIn[i];
+      mom[i] = yIn[i+3];
+      //m_mom_sqrd += mom[i]*mom[i];
+   }
+   /*m_mom = std::sqrt(m_mom_sqrd);
+   for(i = 0; i < 3; i ++){
+      mom[i] /= m_mom;
+   }*/
+   
+   t = yIn[7];
+      
+	for(i = 0; i < 3; i ++){
+		temp_eval_pt[i] = pos[i] + alpha1*Step*mom[i];
+	}
+	for(i = 0; i < 3; i ++){
+		temp_eval_pt[i+3] = mom[i];
+	}
+	temp_eval_pt[7] = t + alpha1*Step;
+	
+	EvaluateRhs(temp_eval_pt, K1);
+
+	for(i = 0; i < 3; i ++){
+		temp_eval_pt[i] = pos[i] + alpha2*Step*mom[i] + Step*Step*beta21*K1[i];
+	}
+	for(i = 0; i < 3; i ++){
+		temp_eval_pt[i+3] = mom[i] + Step*gamma21*K1[i];
+	}
+	temp_eval_pt[7] = t + alpha2*Step;
+   
+	EvaluateRhs(temp_eval_pt, K2);
+
+	for(i = 0; i < 3; i ++){
+		temp_eval_pt[i] = pos[i] + alpha3*Step*mom[i] + Step*Step*
+					(beta31*K1[i] + beta32*K2[i]);
+	}
+	for(i = 0; i < 3; i ++){
+	temp_eval_pt[i+3] = mom[i] + Step*(gamma31*K1[i] + gamma32*K2[i]);
+	}
+	temp_eval_pt[7] = t + alpha3*Step;
+   
+	EvaluateRhs(temp_eval_pt, K3);
+
+	for(i = 0; i < 3; i ++)
+	{
+	// Accumulate increments with proper weights
+
+		yOut[i] = pos[i] + Step*mom[i] + Step*Step*
+				(a1*K1[i] + a2*K2[i] + a3*K3[i]);
+	}
+	for(i = 0; i < 3; i ++){
+		yOut[i+3] = mom[i] + Step*( b1*K1[i] + b2*K2[i] + b3*K3[i] );
+	}
+	
+	//G4double normF = 1. / std::sqrt(yOut[3]*yOut[3] + yOut[4]*yOut[4] + yOut[5]*yOut[5]);
+	//yOut[3] *= normF; yOut[4] *= normF; yOut[5] *= normF;
+	
+	// Nomalization above is commented out because we need to not 
+	// normalize in between the half steps
+	
+	return ;
+}
+
+
+void
+ChawlaSharmaRKNstepper::EvaluateRhs( const G4double y[],
+				           G4double dmom[] ) const
+{ 
+   G4double Point[4] = { y[0], y[1], y[2], y[7] };
+   G4double B[3];
+   m_fEq->GetFieldObj()->GetFieldValue( Point, B );
+   
+   //G4double cof = m_fEq->FCof();
+
+   dmom[0] = (y[4]*B[2] - y[5]*B[1]) ;   // Ax = a*(Vy*Bz - Vz*By)
+   dmom[1] = (y[5]*B[0] - y[3]*B[2]) ;   // Ay = a*(Vz*Bx - Vx*Bz)
+   dmom[2] = (y[3]*B[1] - y[4]*B[0]) ;   // Az = a*(Vx*By - Vy*Bx)
+
+   return ;
+}
+
+G4double ChawlaSharmaRKNstepper::DistChord() const 
+{
+  // Estimate the maximum distance from the curve to the chord
+  //
+  //  We estimate this using the distance of the midpoint to 
+  //  chord (the line between 
+  // 
+  //  Method below is good only for angle deviations < 2 pi, 
+  //   This restriction should not a problem for the Runge cutta methods, 
+  //   which generally cannot integrate accurately for large angle deviations.
+  G4double distLine, distChord; 
+
+  if (fInitialPoint != fFinalPoint) {
+     distLine= G4LineSection::Distline( fMidPoint, fInitialPoint, fFinalPoint );
+     // This is a class method that gives distance of Mid 
+     //  from the Chord between the Initial and Final points.
+
+     distChord = distLine;
+  }else{
+     distChord = (fMidPoint-fInitialPoint).mag();
+  }
+  return distChord;
+}
