67e5749450240b2c4b95841f767ac179f071641b
Somnath Banerjee <hackabot@gmail.com>
2015-05-29 21:30:15 +0530

    Corrected memory leaks causing divergence of y-coordinate

--- test/testSteppers/testH.cc
+++ test/testSteppers/testH.cc
@@ -8,21 +8,25 @@
 #include "G4LineSection.hh"
 #include "G4MagIntegratorStepper.hh"
 #include "DormandPrince745.hh"
+#include "BogackiShampine45.hh"
 #include <iomanip>
-
+#include "G4ClassicalRK4.hh"
+#include "G4SimpleHeum.hh"
+//#include
 //#include <system.h>
 //#include "G4Types.h"
 
 using namespace std;
 using namespace CLHEP;
 
-//Version 2.0 - Includes direct comparison with G4ExactHelix and New 1 new stepper
+//Version 2.0 - Includes direct comparison with G4ExactHelix
 
 /* Stepper No.
     0. G4ExactHelix
     1. G4CashKarpRKF45
     2. BogackiShampine23
     3. DormandPrince745
+    4. BogackiShampine45
  */
 
 
@@ -33,9 +37,9 @@ int main(int argc, char *args[]){
 /* USER SPACE
     - Modify values here */
     
-    int no_of_steps = 100;          //No. of Steps for the stepper
+    int no_of_steps = 1000;         //No. of Steps for the stepper
     int stepper_no = 3;             //Choose stepper no., for refernce see above
-    G4double step_len = 1*mm;   //Step length in milimeters
+    G4double step_len = 10*mm;  //Step length in milimeters
   
     //Set coordinates here
     G4double
@@ -44,19 +48,19 @@ int main(int argc, char *args[]){
     z_pos = 0.,
 
     x_mom = 0.,                 //mom - momentum
-    y_mom = 100.0*gram*m*(1/s),
+    y_mom = 10.,
     z_mom = 0.,
     
     x_field = 0.,               //Uniform Magnetic Field (x,y,z)
     y_field = 0.,
-    z_field = -0.001*tesla ;
+    z_field = -0.1*tesla ;
     
     
     //Set Charge etc.
-    G4double particleCharge = +1.0*coulomb,     // in e+ units
+    G4double particleCharge = +1.0,     // in e+ units
     spin=0.0,                                   // ignore the spin
     magneticMoment= 0.0,                        // ignore the magnetic moment
-    mass = 1*gram;
+    mass = 1;
     
     //Choice of output coordinates
     int
@@ -64,7 +68,7 @@ int main(int argc, char *args[]){
     {
         1 ,  //x_pos
         1 ,  //y_pos
-        0 ,  //z_pos
+        1 ,  //z_pos
         0 ,  //x_mom
         0 ,  //y_mom
         0    //z_mom
@@ -88,13 +92,22 @@ int main(int argc, char *args[]){
     if(argc>1)
         stepper_no = atoi(args[1]);
     if(argc > 2)
-        step_len = stof(args[2]);
+        step_len = (float)(stof(args[2])*mm);
+    if(argc > 3)
+        no_of_steps = atoi(args[3]);
     
-    //Intialising coordinates
+    //Initialising coordinates
     G4double yIn[] = {x_pos,y_pos,z_pos,x_mom,y_mom,z_mom};
     G4double yInX[] = {x_pos,y_pos,z_pos,x_mom,y_mom,z_mom};
-    G4double dydx[] = {0,0,0,0,0,0};    //Setting initial dydx, we calculate it later using RHS
-    G4double yout[6], youtX[6], yerr[6], yerrX[6];
+    
+    
+    //Empty buckets for results
+    G4double dydx[] = {0.,0.,0.,0.,0.,0.},
+        dydxRef[] = {0.,0.,0.,0.,0.,0.},
+        yout[6] = {0.,0.,0.,0.,0.,0.},
+        youtX[6] = {0.,0.,0.,0.,0.,0.},
+        yerr[6] = {0.,0.,0.,0.,0.,0.},
+        yerrX[6] = {0.,0.,0.,0.,0.,0.};
 
     //1. Create a field :
     G4UniformMagField myField(G4ThreeVector(x_field, y_field, z_field));
@@ -110,7 +123,7 @@ int main(int argc, char *args[]){
 
     fEquation->SetChargeMomentumMass( chargeState,
                                 G4ThreeVector(x_field, y_field, z_field).mag(), //momentum magnitude
-                                mass);
+                                mass);//No place fo mass in fEquation though
     
     //Create a stepper :
     G4MagIntegratorStepper *myStepper, *exactStepper;
@@ -126,6 +139,12 @@ int main(int argc, char *args[]){
             break;
         case 3: myStepper = new DormandPrince745(fEquation);
             break;
+        case 4: myStepper = new BogackiShampine45(fEquation);
+            break;
+        case 5: myStepper = new G4ClassicalRK4(fEquation);
+            break;
+        case 6: myStepper = new G4SimpleHeum(fEquation);
+            break;
         default : myStepper = 0 ;
     }
     
@@ -137,15 +156,11 @@ int main(int argc, char *args[]){
     
         cout<<"\n#";
         cout<<setw(7)<<"StepNo";
-    
     for (int i=0; i<6;i++)
         if (columns[i])
-            cout << setw(13) << "yOut[" << i << "]" << setw(13) << "yErr[" << i << "]"
-                             << setw(13) << "yOut-yOutX[" << i << "]";
-
-    
-    //  for (int i=0; i<6;i++) cout << setw(13) <<
-//      for (int i=0; i<6;i++) cout << setw(13) <<
+            cout << setw(13)<< "yOut[" << i << "]"
+                 << setw(13) << "yErr[" << i << "]"
+                 << setw(13) << "yOut-yOutX[" << i << "]";
 
     
         //-> Then print the data
@@ -168,16 +183,16 @@ int main(int argc, char *args[]){
             myStepper->Stepper(yIn,dydx,step_len,yout,yerr);        //call the stepper
             
         
-            exactStepper->RightHandSide(yInX, dydx);                    //compute the value of dydx for the exact stepper
+            exactStepper->RightHandSide(yInX, dydxRef);                 //compute the value of dydx for the exact stepper
                                                                             // ^ Same dydx space being used
-            exactStepper->Stepper(yInX,dydx,step_len,youtX,yerrX);      //call the exact stepper
+            exactStepper->Stepper(yInX,dydxRef,step_len,youtX,yerrX);       //call the exact stepper
 
             
 
         //-> Then print the data
             for(int i=0; i<6;i++)
                 if(columns[i]){
-                    cout<<setw(15)<<yout[i];
+                    cout<<setw(15)<<yout[i]<<setw(15);
                     cout<<setw(15)<<yerr[i];
                     cout<<setw(15)<<yout[i] - youtX[i];
                 }
