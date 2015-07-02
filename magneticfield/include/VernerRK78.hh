26174d51bae03299132affdf6eb9d2dfcd7c09fe
Somnath Banerjee <hackabot@gmail.com>
2015-07-02 21:53:04 +0530

    Interpolation in VernerRK78
    Includes the lower order interpolant given by Verner’s RK 8(7) code

--- magneticfield/include/VernerRK78.hh
+++ magneticfield/include/VernerRK78.hh
@@ -36,14 +36,27 @@ public:
     ~VernerRK78() ;
     
     //Stepper
-    void Stepper( const G4double y[],
+    void Stepper( const G4double yInput[],
                  const G4double dydx[],
                  G4double h,
                  G4double yout[],
                  G4double yerr[] ) ;
     
+    //For Preparing the Interpolant and calculating the extra stages
+    void SetupInterpolate( const G4double yInput[],
+                           const G4double dydx[],
+                           const G4double Step );
+    
+    //For calculating the output at the tau fraction of Step
+    void Interpolate( const G4double yInput[],
+                      const G4double dydx[],
+                      const G4double Step,
+ 	                        G4double yOut[],
+                     		G4double tau );
+    
     G4double  DistChord()   const;
     G4int IntegratorOrder() const { return 7; }
+    G4bool isFSAL() const{ return false;}
     
     VernerRK78(const VernerRK78&);
     VernerRK78& operator=(const VernerRK78&);
@@ -52,6 +65,7 @@ public:
 private:
     
 	   G4double *ak2, *ak3, *ak4, *ak5, *ak6, *ak7, *ak8, *ak9, *ak10, *ak11, *ak12, *ak13,        // for storing intermediate 'k' values in stepper
+    	*ak14, *ak15, *ak16, *ak17, 	//For storing additional stages of interpolants
     *yTemp, *yIn;
     
     G4double fLastStepLength;
