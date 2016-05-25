cfaa9a02f4b526ece8083ccc654c0ac5b37cd498
dmitrySorokin <dmitrii.sorokin@phystech.edu>
2016-05-25 23:58:29 +0300

    add testNTST

---
+++ test/testNTST-2016-04-28/build/CMakeFiles/testNTST.dir/DependInfo.cmake
@@ -0,0 +1,56 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/Dmitry/work/GSoC/testNTST-2016-04-28/src/NTSTBabarEvtReadGenerator.cc" "/home/Dmitry/work/GSoC/testNTST-2016-04-28/build/CMakeFiles/testNTST.dir/src/NTSTBabarEvtReadGenerator.cc.o"
+  "/home/Dmitry/work/GSoC/testNTST-2016-04-28/src/NTSTDetectorConstruction.cc" "/home/Dmitry/work/GSoC/testNTST-2016-04-28/build/CMakeFiles/testNTST.dir/src/NTSTDetectorConstruction.cc.o"
+  "/home/Dmitry/work/GSoC/testNTST-2016-04-28/src/NTSTDetectorMessenger.cc" "/home/Dmitry/work/GSoC/testNTST-2016-04-28/build/CMakeFiles/testNTST.dir/src/NTSTDetectorMessenger.cc.o"
+  "/home/Dmitry/work/GSoC/testNTST-2016-04-28/src/NTSTEventAction.cc" "/home/Dmitry/work/GSoC/testNTST-2016-04-28/build/CMakeFiles/testNTST.dir/src/NTSTEventAction.cc.o"
+  "/home/Dmitry/work/GSoC/testNTST-2016-04-28/src/NTSTEventActionMessenger.cc" "/home/Dmitry/work/GSoC/testNTST-2016-04-28/build/CMakeFiles/testNTST.dir/src/NTSTEventActionMessenger.cc.o"
+  "/home/Dmitry/work/GSoC/testNTST-2016-04-28/src/NTSTFileRead.cc" "/home/Dmitry/work/GSoC/testNTST-2016-04-28/build/CMakeFiles/testNTST.dir/src/NTSTFileRead.cc.o"
+  "/home/Dmitry/work/GSoC/testNTST-2016-04-28/src/NTSTGunGenerator.cc" "/home/Dmitry/work/GSoC/testNTST-2016-04-28/build/CMakeFiles/testNTST.dir/src/NTSTGunGenerator.cc.o"
+  "/home/Dmitry/work/GSoC/testNTST-2016-04-28/src/NTSTGunMessenger.cc" "/home/Dmitry/work/GSoC/testNTST-2016-04-28/build/CMakeFiles/testNTST.dir/src/NTSTGunMessenger.cc.o"
+  "/home/Dmitry/work/GSoC/testNTST-2016-04-28/src/NTSTLooperDeath.cc" "/home/Dmitry/work/GSoC/testNTST-2016-04-28/build/CMakeFiles/testNTST.dir/src/NTSTLooperDeath.cc.o"
+  "/home/Dmitry/work/GSoC/testNTST-2016-04-28/src/NTSTPhysicsList.cc" "/home/Dmitry/work/GSoC/testNTST-2016-04-28/build/CMakeFiles/testNTST.dir/src/NTSTPhysicsList.cc.o"
+  "/home/Dmitry/work/GSoC/testNTST-2016-04-28/src/NTSTPhysicsListMessenger.cc" "/home/Dmitry/work/GSoC/testNTST-2016-04-28/build/CMakeFiles/testNTST.dir/src/NTSTPhysicsListMessenger.cc.o"
+  "/home/Dmitry/work/GSoC/testNTST-2016-04-28/src/NTSTPrimaryGeneratorAction.cc" "/home/Dmitry/work/GSoC/testNTST-2016-04-28/build/CMakeFiles/testNTST.dir/src/NTSTPrimaryGeneratorAction.cc.o"
+  "/home/Dmitry/work/GSoC/testNTST-2016-04-28/src/NTSTPrimaryGeneratorMessenger.cc" "/home/Dmitry/work/GSoC/testNTST-2016-04-28/build/CMakeFiles/testNTST.dir/src/NTSTPrimaryGeneratorMessenger.cc.o"
+  "/home/Dmitry/work/GSoC/testNTST-2016-04-28/src/NTSTRotationMatrix.cc" "/home/Dmitry/work/GSoC/testNTST-2016-04-28/build/CMakeFiles/testNTST.dir/src/NTSTRotationMatrix.cc.o"
+  "/home/Dmitry/work/GSoC/testNTST-2016-04-28/src/NTSTRunAction.cc" "/home/Dmitry/work/GSoC/testNTST-2016-04-28/build/CMakeFiles/testNTST.dir/src/NTSTRunAction.cc.o"
+  "/home/Dmitry/work/GSoC/testNTST-2016-04-28/src/NTSTSteppingAction.cc" "/home/Dmitry/work/GSoC/testNTST-2016-04-28/build/CMakeFiles/testNTST.dir/src/NTSTSteppingAction.cc.o"
+  "/home/Dmitry/work/GSoC/testNTST-2016-04-28/testNTST.cc" "/home/Dmitry/work/GSoC/testNTST-2016-04-28/build/CMakeFiles/testNTST.dir/testNTST.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4UI_USE"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4_STORE_TRAJECTORY"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/Dmitry/geant/geant4.10.02-install/include/Geant4"
+  "../include"
+  "/usr/include/qt5"
+  "/usr/include/qt5/QtWidgets"
+  "/usr/include/qt5/QtGui"
+  "/usr/include/qt5/QtCore"
+  "/usr/lib64/qt5/./mkspecs/linux-g++"
+  "/usr/include/qt5/QtPrintSupport"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
