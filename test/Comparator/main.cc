99767ed124ca11a3082618060ea952ea26435122
dmitrySorokin <dmitrii.sorokin@phystech.edu>
2016-08-01 07:44:17 +0300

    add testNTST and Comparator

---
+++ test/Comparator/main.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+#include "Comparator.hh"
+#include "G4UniformMagField.hh"
+#include "G4QuadrupoleMagField.hh"
+
+#include "G4ClassicalRK4.hh"
+#include "G4RKG3_Stepper.hh"
+#include "G4ExactHelixStepper.hh"
+#include "G4CashKarpRKF45.hh"
+#include "G4ExplicitEuler.hh"
+
+#include "G4CachedMagneticField.hh"
+
+using namespace std;
+using namespace CLHEP;
+
+
+int main(){
+    G4DynamicParticle* dynParticle = new G4DynamicParticle(G4Proton::Definition(),G4ThreeVector(1,0.01,0.01),1*GeV);
+    //G4MagneticField* field = new G4UniformMagField(G4ThreeVector(0, 0, 1*tesla));
+    G4MagneticField* field = new G4QuadrupoleMagField(0.01*tesla/meter);
+    Comparator comparator(dynParticle,new G4CachedMagneticField(field,0));
+    //comparator.Compare<G4ExplicitEuler,G4CashKarpRKF45>(10*cm,10000,false,Verbose);
+    comparator.CompareWithBS<G4CashKarpRKF45>(1000*m,Verbose);
+
+    return 0;
+}
