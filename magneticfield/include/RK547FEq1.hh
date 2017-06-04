9de2d6a4fa619c20f7604dbaa6837e1416716f13
Dmitry Sorokin <dmitrii.sorokin.phystech.edu>
2017-06-04 18:55:41 +0300

    improve RK547FEq1 implementation

--- magneticfield/include/RK547FEq1.hh
+++ magneticfield/include/RK547FEq1.hh
@@ -9,46 +9,30 @@ class RK547FEq1 : public G4MagIntegratorStepper
 public:
     RK547FEq1 (G4EquationOfMotion *EqRhs, G4int integrationVariables = 6);
 
-    void Stepper (const G4double yInput[],
-                  const G4double dydx[],
-                        G4double hstep,
-                        G4double yOutput[],
-                        G4double yError[]);
+    virtual void Stepper (const G4double yInput[],
+                          const G4double dydx[],
+                          G4double hstep,
+                          G4double yOutput[],
+                          G4double yError[]) override;
 
     RK547FEq1 (const RK547FEq1&) = delete;
     RK547FEq1& operator = (const RK547FEq1&) = delete;
 
-    G4double  DistChord()   const;
-    G4int IntegratorOrder() const { return 4; }
-
+    virtual G4double DistChord() const override;
+    virtual G4int IntegratorOrder() const override { return 4; }
 
 private:
-    void saveStep(const G4double yInput[],
-                  const G4double dydx[],
-                  const G4double hstep,
-                  const G4double yOutput[]);
-
     void makeStep(const G4double yInput[],
                   const G4double dydx[],
                   const G4double hstep,
                         G4double yOutput[],
-                        G4double yError[]) const;
-
-    mutable G4double ak2_[G4FieldTrack::ncompSVEC],
-                     ak3_[G4FieldTrack::ncompSVEC],
-                     ak4_[G4FieldTrack::ncompSVEC],
-                     ak5_[G4FieldTrack::ncompSVEC],
-                     ak6_[G4FieldTrack::ncompSVEC],
-                     ak7_[G4FieldTrack::ncompSVEC],
-
-                     yMid_ [G4FieldTrack::ncompSVEC],
-                     yErr_[G4FieldTrack::ncompSVEC],
-                     yTemp_[G4FieldTrack::ncompSVEC];
+                        G4double* dydxOutput = nullptr,
+                        G4double* yError = nullptr) const;
 
     G4double yIn_[G4FieldTrack::ncompSVEC],
-             yOut_[G4FieldTrack::ncompSVEC],
              dydx_[G4FieldTrack::ncompSVEC],
-             dydxNext_[G4FieldTrack::ncompSVEC];
+             yOut_[G4FieldTrack::ncompSVEC],
+             dydxOut_[G4FieldTrack::ncompSVEC];
     G4double hstep_;
 };
 
--- magneticfield/src/RK547FEq1.cc
+++ magneticfield/src/RK547FEq1.cc
@@ -7,7 +7,7 @@
 //  3/5 |      91/500    -27/100    78/125    8/125
 //   1  |      -11/20    27/20      12/5      -36/5    5
 //   1  |      1/12      0          27/32     -4/3     125/96    5/48
-//-------------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------
 //             1/12      0          27/32     -4/3     125/96    5/48    0
 //             2/15      0          27/80     -2/15    25/48     1/24    1/10
 
@@ -15,32 +15,34 @@
 #include "G4LineSection.hh"
 #include "Utils.hh"
 
+namespace {
+    void copyArray(G4double dst[], const G4double src[]) {
+        memcpy(dst, src, sizeof(G4double) * G4FieldTrack::ncompSVEC);
+    }
+}
 
 RK547FEq1::RK547FEq1(G4EquationOfMotion *EqRhs, G4int integrationVariables)
    : G4MagIntegratorStepper(EqRhs, integrationVariables)
 {
 }
 
-void RK547FEq1::saveStep(const G4double yInput[],
+void RK547FEq1::makeStep(const G4double yInput[],
                          const G4double dydx[],
                          const G4double hstep,
-                         const G4double yOutput[])
+                         G4double yOutput[],
+                         G4double* dydxOutput,
+                         G4double* yError) const
 {
+    G4double yTemp[G4FieldTrack::ncompSVEC];
+    for (int i = GetNumberOfVariables(); i < GetNumberOfStateVariables(); ++i){
+        yOutput[i] = yTemp[i] = yInput[i];
+    }
 
-    memcpy (yIn_, yInput, sizeof(G4double) * GetNumberOfStateVariables());
-    memcpy (dydx_, dydx, sizeof(G4double) * GetNumberOfStateVariables());
-    hstep_ = hstep;
-    memcpy (yOut_, yOutput, sizeof(G4double) * GetNumberOfStateVariables());
-    memcpy (dydxNext_, ak7_, sizeof(G4double) * GetNumberOfStateVariables());
-}
-
-void RK547FEq1::makeStep(const G4double yInput[],
-                        const G4double dydx[],
-                        const G4double hstep,
-                        G4double yOutput[],
-                        G4double yError[]) const
-{
-    //TODO copy time array[7], copy yInput
+    G4double ak2[G4FieldTrack::ncompSVEC],
+             ak3[G4FieldTrack::ncompSVEC],
+             ak4[G4FieldTrack::ncompSVEC],
+             ak5[G4FieldTrack::ncompSVEC],
+             ak6[G4FieldTrack::ncompSVEC];
 
     const G4double
        b21 = 2./9.,
@@ -65,44 +67,41 @@ void RK547FEq1::makeStep(const G4double yInput[],
 
     //RightHandSide(yInput, dydx);
     for(int i = 0; i < GetNumberOfVariables(); ++i)
-        yTemp_[i] = yInput[i] + hstep * b21 * dydx[i];
+        yTemp[i] = yInput[i] + hstep * b21 * dydx[i];
 
-    //TODO declare RightHandSide - const!
-    G4MagIntegratorStepper* super = const_cast<RK547FEq1*>(this);
-
-    super->RightHandSide(yTemp_, ak2_);
+    RightHandSide(yTemp, ak2);
     for(int i = 0; i < GetNumberOfVariables(); ++i)
-        yTemp_[i] = yInput[i] + hstep * (b31 * dydx[i] + b32 * ak2_[i]);
+        yTemp[i] = yInput[i] + hstep * (b31 * dydx[i] + b32 * ak2[i]);
 
-    super->RightHandSide(yTemp_, ak3_);
+    RightHandSide(yTemp, ak3);
     for(int i = 0;i < GetNumberOfVariables(); ++i)
-        yTemp_[i] = yInput[i] + hstep * (b41 * dydx[i] + b42 * ak2_[i] +
-                                        b43 * ak3_[i]);
+        yTemp[i] = yInput[i] + hstep * (b41 * dydx[i] + b42 * ak2[i] +
+                                        b43 * ak3[i]);
 
-    super->RightHandSide(yTemp_, ak4_);
+    RightHandSide(yTemp, ak4);
     for(int i = 0; i < GetNumberOfVariables(); ++i)
-        yTemp_[i] = yInput[i] + hstep * (b51 * dydx[i] + b52 * ak2_[i] +
-                                        b53 * ak3_[i] + b54 * ak4_[i]);
+        yTemp[i] = yInput[i] + hstep * (b51 * dydx[i] + b52 * ak2[i] +
+                                        b53 * ak3[i] + b54 * ak4[i]);
 
-    super->RightHandSide(yTemp_, ak5_);
+    RightHandSide(yTemp, ak5);
     for(int i = 0; i < GetNumberOfVariables(); ++i)
-        yTemp_[i] = yInput[i] + hstep * (b61 * dydx[i] + b62 * ak2_[i] +
-                                        b63 * ak3_[i] + b64 * ak4_[i] +
-                                        b65 * ak5_[i]);
+        yTemp[i] = yInput[i] + hstep * (b61 * dydx[i] + b62 * ak2[i] +
+                                        b63 * ak3[i] + b64 * ak4[i] +
+                                        b65 * ak5[i]);
 
-    super->RightHandSide(yTemp_, ak6_);
+    RightHandSide(yTemp, ak6);
     for(int i = 0; i < GetNumberOfVariables(); ++i)
-        yOutput[i] = yInput[i] + hstep * (b71 * dydx[i] + b72 * ak2_[i] +
-                                          b73 * ak3_[i] + b74 * ak4_[i] +
-                                          b75 * ak5_[i] + b76 * ak6_[i]);
+        yOutput[i] = yInput[i] + hstep * (b71 * dydx[i] + b72 * ak2[i] +
+                                          b73 * ak3[i] + b74 * ak4[i] +
+                                          b75 * ak5[i] + b76 * ak6[i]);
 
-    super->RightHandSide(yOutput, ak7_);
-    for(int i = 0; i < GetNumberOfVariables(); ++i)
-        yError[i] = hstep * (dc1 * dydx[i] + dc2 * ak2_[i] + dc3 * ak3_[i] +
-                             dc4 * ak4_[i] + dc5 * ak5_[i] + dc6 * ak6_[i] +
-                             dc7 * ak7_[i]);
-
-    //TODO use ak7_ as dydx for next step (FSAL property)
+    if (dydxOutput && yError) {
+        RightHandSide(yOutput, dydxOutput);
+        for(int i = 0; i < GetNumberOfVariables(); ++i)
+            yError[i] = hstep * (dc1 * dydx[i] + dc2 * ak2[i] + dc3 * ak3[i] +
+                                 dc4 * ak4[i] + dc5 * ak5[i] + dc6 * ak6[i] +
+                                 dc7 * dydxOutput[i]);
+    }
 }
 
 void RK547FEq1::Stepper(const G4double yInput[],
@@ -111,8 +110,13 @@ void RK547FEq1::Stepper(const G4double yInput[],
                         G4double yOutput[],
                         G4double yError[])
 {
-    makeStep(yInput, dydx, hstep, yOutput, yError);
-    saveStep(yInput, dydx, hstep, yOutput);
+    copyArray(yIn_, yInput);
+    copyArray(dydx_, dydx);
+    hstep_ = hstep;
+
+    makeStep(yInput, dydx, hstep, yOutput, dydxOut_, yError);
+
+    copyArray(yOut_, yOutput);
 }
 
 G4double RK547FEq1::DistChord() const
@@ -120,9 +124,9 @@ G4double RK547FEq1::DistChord() const
     const G4ThreeVector begin = makeVector(yIn_, Type::Position);
     const G4ThreeVector end = makeVector(yOut_, Type::Position);
 
-    makeStep(yIn_, dydx_, hstep_ / 2., yMid_, yErr_);
-
-    const G4ThreeVector mid = makeVector(yMid_, Type::Position);
+    G4double yMid[G4FieldTrack::ncompSVEC];
+    makeStep(yIn_, dydx_, hstep_ / 2., yMid);
+    const G4ThreeVector mid = makeVector(yMid, Type::Position);
 
     return G4LineSection::Distline(mid, begin, end);
 }
